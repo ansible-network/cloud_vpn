2572651551607a5efe18fefb6444aac7caafaf09
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2018-05-21 12:51:53 +0200

    Do not check for host key and bump persistent command timeout on run_playbook

--- templates/run_playbook.sh
+++ templates/run_playbook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i inventory playbook.yaml -vvv
+ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=60 ansible-playbook -i inventory playbook.yaml -vvv
