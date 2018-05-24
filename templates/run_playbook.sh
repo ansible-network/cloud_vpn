7c670e4199f1a6a94fa7f80ac39ebe03482c48c8
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2018-05-24 15:39:36 +0200

    Create play structure ansible-runner compatible

--- templates/run_playbook.sh
+++ templates/run_playbook.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=60 ansible-playbook -i inventory playbook.yaml -vvv
+cd project
+ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=60 ansible-playbook -i ../inventory playbook.yaml -vvv
