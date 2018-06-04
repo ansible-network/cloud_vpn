1f7fdbd5eb69a6cb0fa20c3af6d35199b0cfd6d5
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2018-06-04 19:47:18 +0200

    Refactor to support peer types
    Rather than tie flow to layout (aws_vpn_to_ios, aws_vpn_to_vyos, etc),
    add cloud_vpn_peer_type_a/b vars and add templates to each case.
    This allows greater code reusability.

--- templates/run_playbook.sh
+++ templates/run_playbook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd project
-ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=60 ansible-playbook -i ../inventory playbook.yaml -vvv
+ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=60 ansible-playbook -i ../inventory main.yaml -vvv
