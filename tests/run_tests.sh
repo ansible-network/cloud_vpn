5cf01393a9b57a0dad830fcabfe22c60ea9a648a
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2018-07-03 16:40:31 +0200

    Fix bashate errors

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-  ansible-playbook test.yaml -e "@secrets.yaml"
+    ansible-playbook test.yaml -e "@secrets.yaml"
 else
-  ansible-playbook test.yaml -e "@$1"
+    ansible-playbook test.yaml -e "@$1"
 fi
